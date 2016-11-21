bde5ecc66d68801359e3771c2c5d18dabf5aec22
Nov4213 <wenborao@163.com>
2016-11-21 17:44:51 +0800

    ChatGroup

---
+++ src/main/java/com/bjlx/QinShihuang/controller/ChatgroupController.java
@@ -0,0 +1,12 @@
+package com.bjlx.QinShihuang.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * qunzu
+ * Created by Nov on 2016/11/6.
+ */
+@Controller
+public class ChatgroupController {
+
+}
