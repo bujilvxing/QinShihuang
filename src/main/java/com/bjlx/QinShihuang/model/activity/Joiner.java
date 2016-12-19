df23d1b710453806cd2b4cef30f27018a452b765
ECHO-FOLLOW-HEART <pengyt19890703@163.com>
2016-12-19 18:06:22 +0800

    活动添加name字段

--- src/main/java/com/bjlx/QinShihuang/model/activity/Joiner.java
+++ src/main/java/com/bjlx/QinShihuang/model/activity/Joiner.java
@@ -15,11 +15,17 @@ public class Joiner extends Contact {
 
     @Transient
     public final static String fd_userId = "userId";
-
+    @Transient
+    public final static String fd_name = "name";
     /**
      * 用户id
      */
     private Long userId;
+
+    /**
+     * 参与者姓名
+     */
+    private String name;
 
     public Long getUserId() {
         return userId;
@@ -29,11 +35,19 @@ public class Joiner extends Contact {
         this.userId = userId;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Joiner() {
 
     }
 
-    public Joiner(Long userId, List<String> phoneList, List<String> cellphoneList, String qq, String weixin, String sina, String fax, String email, String website) {
+    public Joiner(Long userId, String name, List<String> phoneList, List<String> cellphoneList, String qq, String weixin, String sina, String fax, String email, String website) {
         super(phoneList, cellphoneList, qq, weixin, sina, fax, email, website);
         this.userId = userId;
     }
